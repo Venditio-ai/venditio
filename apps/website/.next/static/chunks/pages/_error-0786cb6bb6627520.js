56f7f7cef7732c96c20f707d74b9ed485e45e043
quancore <barann.nama@gmail.com>
2025-04-08 03:25:36 +0200

    Re-add website directory as regular directory in monorepo

---
+++ apps/website/.next/static/chunks/pages/_error-0786cb6bb6627520.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{1389:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(4473)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(1389)),_N_E=_.O()}]);
