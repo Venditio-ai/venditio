d9b57ca903b44a81522448bfc98e3867a02d0af4
quancore <barann.nama@gmail.com>
2025-04-08 03:25:28 +0200

    Remove website directory to prepare for monorepo management

--- apps/website/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['via.placeholder.com'],
-  },
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
